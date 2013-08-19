4a5654aa6894cc5096df1170539acbd4e0049113
Julien Simon <julien@julien.org>
2013-08-19 13:02:41 +0200

    Homework done and tested

---
+++ week3/hw3-1/src/main/java/com/tengen/RemoveGrade.java
@@ -0,0 +1,38 @@
+package com.tengen;
+
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+public class RemoveGrade {
+    public static void main(String[] args) throws UnknownHostException {
+        MongoClient client = new MongoClient();
+
+        DB database = client.getDB("school");
+        DBCollection collection = database.getCollection("students");
+
+        DBObject query = new BasicDBObject("scores.type", "homework");
+        DBCursor c = collection.find(query);
+
+        while (c.hasNext()) {
+            double lowest = 100;
+
+            DBObject student = c.next();
+            ArrayList<BasicDBObject> scores = (ArrayList) student.get("scores");
+
+            for (BasicDBObject dbo : scores) {
+                if (dbo.getString("type").equals("homework")) {
+                    double score = dbo.getDouble("score");
+                    if (score < lowest) {
+                        lowest = score;
+                    }
+                }
+            }
+
+            DBObject match = new BasicDBObject("_id", student.get("_id"));
+            DBObject update = new BasicDBObject("scores", new BasicDBObject("type", "homework").append("score", lowest));
+            collection.update(match, new BasicDBObject("$pull", update));
+        }
+    }
+}
